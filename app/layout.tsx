55727ea0137dd33eae6c6ff107733557f992345a
William Phong <williamphong@users.noreply.github.com>
2024-06-15 13:36:01 -0700

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import { Inter, Montserrat } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -54,6 +55,7 @@ export default function RootLayout({
         <div>
           {children}
           <Analytics />
+          <SpeedInsights />
         </div>
       </body>
     </html>
