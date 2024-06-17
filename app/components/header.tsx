8925f19fb27846845be42f5833503c805424e1ab
William Phong <williamphong@users.noreply.github.com>
2024-06-17 16:56:57 -0700

    removed unnecessary, updated eslint config

--- app/components/header.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return <header className="relative z-[999]"></header>;
-}
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,15 +1,19 @@
-// pages/index.tsx
+import Image from 'next/image';
+import React from 'react';
 
-import Link from 'next/link';
-
-const Gallery = () => {
+export default function Home() {
   return (
-    <div>
-      <h1>Welcome to My Website</h1>
-
-      <p>This is the default page of your Next.js application.</p>
+    <div className="mx-auto flex min-h-screen max-w-screen-xl items-center justify-center px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="text-center">
+        <h1 className="text-4xl">COMING SOON</h1>
+        <Image
+          src="/images/please_do_not_the_cat.png"
+          alt="Description of the image"
+          width={300}
+          height={570}
+          className="pt-24"
+        />
+      </div>
     </div>
   );
-};
-
-export default Gallery;
+}
