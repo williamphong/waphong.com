dc7e5b28bf3b2b119a2e837572159ee012fd0042
William Phong <williamphong@users.noreply.github.com>
2025-05-09 14:42:54 -0700

    fixing img link + hover arrow

--- app/(portfolio)/_components/ProjectList.tsx
+++ app/(portfolio)/_components/ProjectList.tsx
@@ -102,6 +102,19 @@ export const ProjectList = () => {
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                     <span>{project.title}</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="ml-1 inline-block h-4 w-4 shrink-0 translate-y-px transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
                   </a>
                 </h3>
 
--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -119,13 +119,28 @@ export default function Home() {
           <br></br>
           <p className="mb-4 text-slate-400">
             <a
-              className="dark:text-rp-rose text-rpd-rose dark:focus-visible:text-rp-love focus-visible:text-rpd-love font-medium hover:underline hover:underline-offset-4"
+              className="dark:text-rp-rose text-rpd-rose dark:focus-visible:text-rp-love focus-visible:text-rpd-love group/link font-medium hover:underline hover:underline-offset-4"
               href="/files/resume.pdf"
               target="_blank"
               rel="noreferrer noopener"
               aria-label="resume"
             >
-              View my full resume here ↗
+              <span className="inline-block">
+                View my full resume here{' '}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="ml-1 inline-block h-4 w-4 shrink-0 translate-y-px transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>{' '}
+              </span>
             </a>
           </p>
         </section>
